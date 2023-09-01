065ed71db74ec44105914d12972ca080495a1564
Ryan Clarke <ryanclarke999@gmail.com>
2023-09-01 13:44:18 +0100

    Changed the code to implement a function over two do while loops.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,36 +1,27 @@
 ﻿
 using System;
 
-namespace CarpetCalculator 
+namespace CarpetCalculator
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static double GetValidInput(string prompt)
+        // below is creating the variables for the function.To check the input is valid. The double value is used to store valid inputs to assign to the vairables in the calculation
+        // later. While the Bool is used to figure out if the input is valid or not.
         {
-            //Declaring the constant variable for the pricing of the room.
-
-            const double Squarefootcost = 1.55;
-
-            // Below I Declare the Variables for the Height and width of the room for the user to input the values for.
-            Double Height;
-            Double Width;
-            Double Cost;
+            double value;
             bool isValidInput = false;
-            // The first line is to inform the user how much the carpet costs per Square feet. I use a line break to make it neater.
-            Console.WriteLine("The Cost per Square foot is £{0}",Squarefootcost);
-            Console.WriteLine();
 
-            // Next I get the users to input their values below I have created two do loops. One for height and one for width. The loop checks if the input for the user can be
-            // converted into a double. If so the value is outputted into the varable for the dimension required. If not it prompts the user to use a valid imput. 
-
-
+            // below is the do while loop that checks if the users inputs are valid. It attempts to convert the string input into a double and out put it into the value vairable.
+            // if it can do this it sends the stored value over to vairable requesting it in this case it will first be height then width. if it cannot convert the string input into
+            // a double then it will sen the user a prompt saying invalid input and request them to replace their input. 
             do
             {
-                Console.WriteLine("Please input the Height of the room in Ft");
+                Console.WriteLine(prompt);
                 string input = Console.ReadLine();
 
                 // Check if input can be parsed as a double
-                if (double.TryParse(input, out Height))
+                if (double.TryParse(input, out value))
                 {
                     isValidInput = true; // Exit the loop if valid input is provided
                 }
@@ -41,40 +32,23 @@ namespace CarpetCalculator
                 }
             } while (!isValidInput);
 
+            return value;
+        }
 
-            Console.WriteLine("The height of the room is " + Height + "Ft");
+        static void Main()
+        {
+            // below i declare the vairables used for the calculations for the carpet and the ones the value sends its stored inputs to. In this case its Height and width.
+            // then i create the cost vairable that times the inputs collected by the user against the constant double of square foot to work out the cost of the job before.
+            // outputting it to the user. 
+            const double Squarefootcost = 1.55;
+            double height = GetValidInput("Please input the Height of the room in Ft");
+            double width = GetValidInput("Please input the Width of the room in Ft");
+            double cost = (height * width) * Squarefootcost;
+            Console.WriteLine("The height of the room is " + height + " Ft");
+            Console.WriteLine("The width of the room is " + width + " Ft");
             Console.WriteLine();
-           
-            do
-            {
-                Console.WriteLine("Please input the Width of the room in Ft");
-                string input = Console.ReadLine();
+            Console.WriteLine("The Cost to Carpet this room is £{0}", cost);
 
-                // Check if input can be parsed as a double
-                if (double.TryParse(input, out Width))
-                {
-                    isValidInput = true; // Exit the loop if valid input is provided
-                }
-                else
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Invalid input. Please enter a valid number.");
-                }
-            } while (!isValidInput);
-
-
-            Console.WriteLine("The height of the room is " + Height + "Ft");
-            Console.WriteLine("The width of the room is " + Width + "Ft");
-            Console.WriteLine();
-
-
-            // After the user has imputted the vairable they are calculated below and the price is outputted to the user.  
-
-            Cost = (Height * Width) * Squarefootcost;
-            Console.WriteLine();
-            Console.WriteLine("The Cost to Carpet this room is £{0}", Cost);
-
-           
 
         }
     }
